90788f5d7dad3534c327e8cd6028d1c7c402c377
Samantha brake <sammy.brake@gmail.com>
2018-11-17 16:18:25 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,9 @@
 class Actor < ActiveRecord::Base
   has_many :shows
   has_many :characters, through: :shows
+
+  def full_name
+    self.first_name
+    self.last_name
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 6) do
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.integer "show_id"
+    t.integer "actor_id"
     t.string "catchphrase"
   end
 
