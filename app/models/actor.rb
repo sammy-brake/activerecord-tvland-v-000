6aad23bbf778ff15a3014fcc3ecf332e6f34352e
Samantha brake <sammy.brake@gmail.com>
2018-11-17 16:30:17 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,8 +3,7 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    self.first_name
-    self.last_name
+    return "#{self.first_name}  #{self.last_name}"
 
   end
 
